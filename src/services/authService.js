1f4dc763440da3380b81a88b4ca782cbc0ef1f81
BlagoChavdarov <95088243+BlagoChavdarov@users.noreply.github.com>
2021-12-14 01:04:23 +0200

    change fetch request to asp fix CORS error

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,39 +1,42 @@
-const baseUrl = 'http://netsurf2.dev.dotcubes.com/site-api/dispatch_request.asp?method=';
-
-export const loginCheck = async (email,password,user_nm) => {
-    let res = await fetch('https://unipek.dev.prim.bg/EXECUTE/api/RPC.cash.CashApi.getAllItems?token=f37x4acf66gl2d82b2d107850108g51s&cash_id=0020',{
-        method: 'POST',  
-        headers: {
-            'Access-Control-Allow-Origin':'https://unipek.dev.prim.bg',
-            'content-type': 'application/json'
-        },
-    });
-    //let res = await fetch('https://swapi.dev/api/people/1/?format=json');
-    let jsonResult = await res.json();
-    
-  return res.json();
-}
+const baseUrl = 'http://netsurf2.dev.dotcubes.com/site-api/dispatch_request.php?method=';
 
 export const login = async (email, password,user_nm) => {
     
-    let res = await fetch(`${baseUrl}login`, {
-        method: 'POST',
-        mode: 'no-cors', 
-        headers: {
-            'Access-Control-Allow-Origin':'http://netsurf2.dev.dotcubes.com',
-            'content-type': 'text/plain'
-        },
-        body: JSON.stringify({ email, password,user_nm })
-    });
-    
-    let jsonResult = await res.json();
+    console.log("email:"+email);
+   
     
 
-    if (res.ok) {
-        return jsonResult;
-    } else {
-        throw jsonResult.message;
-    }
+
+    let response = await fetch(`http://netsurf2.dev.dotcubes.com/site-api/dispatch_request.asp?method=login`,{
+        method: 'POST',
+        body: JSON.stringify({ email, password,user_nm })
+    });
+
+    let pets = await response.json();
+    console.info(pets);
+    let result = Object.values(pets)
+
+ 
+    console.info(result);
+    return result;
+    // let res = await fetch(baseUrl+'login', {
+    //     method: 'POST',
+    //     mode: 'no-cors', 
+    //     headers: {
+    //         'Access-Control-Allow-Origin':'http://netsurf2.dev.dotcubes.com',
+    //         'content-type': 'application/json'
+    //     },
+    //     body: JSON.stringify({ email, password,user_nm })
+    // });
+    // console.info(res);
+    // let jsonResult = await res.json();
+    
+
+    // if (res.ok) {
+    //     return jsonResult;
+    // } else {
+    //     throw jsonResult.message;
+    // }
 };
 
 export const register = (email, password) => {
